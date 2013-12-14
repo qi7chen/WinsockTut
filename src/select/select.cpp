59cf90030b305c389a0c1acf75ea1d2af0e0728e
johnnie chan <ichenq@gmail.com>
2013-12-15 01:33:45 +0800

    select模型

--- src/select/select.cpp
+++ src/select/select.cpp
@@ -1,112 +1,34 @@
-﻿//  A simple echo server use select model
-//  by ichenq@gmail.com 
-//  Oct 19, 2011
+﻿/**
+ *  @file   select.cpp
+ *  @author ichenq@gmail.com
+ *  @date   Oct 19, 2011
+ *  @brief  使用select模型实现的简单Echo Server
+ *			为每个客户端连接创建一个线程
+ */
 
 #include "../common/utility.h"
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-#include <algorithm>
-
-#pragma warning(disable: 4127)
-#pragma comment(lib, "ws2_32")
+#include <assert.h>
+#include <set>
 
 
 
-SOCKET  create_listen_socket(const _tstring& strHost, const _tstring& strPort);
+#pragma warning(disable:4127)
 
-// event handlers
-void    on_close(SOCKET sockfd, SOCKET* socklist, int* count_ptr);
-bool    on_accept(SOCKET sockfd, SOCKET* socklist, int* count_ptr);
-bool    on_recv(SOCKET sockfd);
+// 所有的客户端套接字
+std::set<SOCKET>    g_total_sockets;
 
 
-// initialize winsock and other environment
-static global_init g_global_init;
-
-// main entry
-int _tmain(int argc, TCHAR* argv[])
-{
-    if (argc != 3)
-    {
-        _tprintf(_T("Usage: $program $host $port"));
-        return 1;
-    }
-
-    SOCKET sockfd = create_listen_socket(argv[1], argv[2]);
-    if (sockfd == INVALID_SOCKET)
-    {
-        return 1;
-    }
-    
-    // total socket descriptor
-    SOCKET socklist[FD_SETSIZE] = {};
-
-    // we only concern readable socket set
-    FD_SET readset = {}; 
-
-    // how many client sockets are avaliable
-    int total_count = 0;
-
-    for (;;)
-    {
-        FD_ZERO(&readset);
-        FD_SET(sockfd, &readset); // add listen socket
-        for (int i = 0; i < total_count; ++i)
-        {
-            FD_SET(socklist[i], &readset); // add client sockets
-        }
-
-        // 50 ms
-        timeval timeout = {0, 500*1000};
-        int nready = select(0, &readset, NULL, NULL, &timeout);
-        if (nready == SOCKET_ERROR)
-        {
-            _tprintf(_T("select() failed, %s"), LAST_ERROR_MSG);
-            break;
-        }
-        if (nready == 0) // time limit expired, timer callback can be handled here
-        {            
-            continue;
-        }
-
-        // check one by one
-        for (int i = 0; i < nready; ++i)
-        {
-            if (FD_ISSET(socklist[i], &readset))
-            {
-                if (!on_recv(socklist[i]))
-                {
-                    // recv failed, close the socket
-                    on_close(socklist[i], socklist, &total_count);
-                }
-            }
-        }
-
-        // new accepted socket
-        if (FD_ISSET(sockfd, &readset))
-        {
-            on_accept(sockfd, socklist, &total_count);
-        }
-    }
-
-    closesocket(sockfd); // close the listen socket
-    return 0;
-}
-
-
-//////////////////////////////////////////////////////////////////////////
-// handlers
-
+// 处理接收数据
 bool on_recv(SOCKET sockfd)
 {
-    char buf[BUFE_SIZE];
-    int bytes = recv(sockfd, buf, BUFE_SIZE, 0);
+    char buf[BUFSIZ];
+    int bytes = recv(sockfd, buf, BUFSIZ, 0);
     if (bytes == SOCKET_ERROR)
     {
-        _tprintf(_T("recv() failed, %s"), LAST_ERROR_MSG);
+        fprintf(stderr, ("recv() failed, %s"), LAST_ERROR_MSG);
         return false;
     }
-    if (bytes == 0) // gracefully closed
+    if (bytes == 0)
     {
         return false;
     }
@@ -114,19 +36,20 @@ bool on_recv(SOCKET sockfd)
     bytes = send(sockfd, buf, bytes, 0);
     if (bytes == SOCKET_ERROR)
     {
-        _tprintf(_T("send() failed, %s"), LAST_ERROR_MSG);
+        fprintf(stderr, ("send() failed, %s"), LAST_ERROR_MSG);
         return false;
     }
 
     return true;
 }
 
-bool on_accept(SOCKET sockfd, SOCKET* socklist, int* count_ptr)
+// 处理客户端连入
+bool on_accept(SOCKET sockfd)
 {
-    assert(count_ptr);
-    if (*count_ptr == FD_SETSIZE - 1)
+    // 达到上限
+    if (g_total_sockets.size() == FD_SETSIZE - 1)
     {
-        _tprintf(_T("got the 64 limit.\n"));
+        fprintf(stderr, ("got the 64 limit.\n"));
         return false;
     }
 
@@ -135,88 +58,163 @@ bool on_accept(SOCKET sockfd, SOCKET* socklist, int* count_ptr)
     int socknew = accept(sockfd, (sockaddr*)&addr, &addrlen);
     if (socknew == INVALID_SOCKET)
     {
-        _tprintf(_T("accept() failed, %s"), LAST_ERROR_MSG);
+        fprintf(stderr, ("accept() failed, %s"), LAST_ERROR_MSG);
         return false;
     }
 
-    // set to non-blocking mode
+    // 设置为非阻塞模式
     ULONG nonblock = 1;
     if (ioctlsocket(socknew, FIONBIO, &nonblock) == SOCKET_ERROR)
     {
-        _tprintf(_T("ioctlsocket() failed, %s"), LAST_ERROR_MSG);
+        fprintf(stderr, ("ioctlsocket() failed, %s"), LAST_ERROR_MSG);
         closesocket(socknew);
         return false;
     }
 
-    _tprintf(_T("socket %d accepted at %s.\n"), socknew, Now().data());
+    fprintf(stdout, ("socket %d accepted at %s.\n"), socknew, Now().data());
 
-    socklist[*count_ptr] = socknew;
-    (*count_ptr)++;
+    g_total_sockets.insert(socknew);
     return true;
 }
 
-void on_close(SOCKET sockfd, SOCKET* socklist, int* count_ptr)
+// 处理客户端连接关闭
+void on_close(SOCKET sockfd)
 {
-    assert(count_ptr);
-    _tprintf(_T("socket %d closed at %s.\n"), sockfd, Now().data());
-
-    // find index of this socket
-    SOCKET* listend = socklist + (*count_ptr+1);
-    bool removed = (std::remove(socklist, listend, sockfd) != listend);
-    if (!removed)
-    {
-        _tprintf(_T("socket %d not found in list.\n"), sockfd);
-    }
-    else
-    {
-        closesocket(sockfd);
-        (*count_ptr)--;
-    }
+    fprintf(stdout, ("socket %d closed at %s.\n"), sockfd, Now().data());
+    closesocket(sockfd);
 }
 
-
-SOCKET  create_listen_socket(const _tstring& strHost, const _tstring& strPort)
+// 创建监听套接字
+SOCKET  create_listen_socket(const char* host, const char* port)
 {
-    sockaddr_in addr = {};
-    const _tstring& strAddr = strHost + _T(":") + strPort;
-    if (!StringToAddress(strAddr, &addr))
+    addrinfo* aiList = NULL;
+    addrinfo hints = {};
+    hints.ai_family = AF_INET;          // TCP v4
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+    int error = getaddrinfo(host, port, &hints, &aiList);
+    if (error != 0)
     {
-        _tprintf(_T("cannot convert '%s' to socket address, %s"), strAddr.data(), LAST_ERROR_MSG);
+        fprintf(stderr, ("getaddrinfo() failed, %s:%s, %s.\n"), host, port, gai_strerror(error));
         return INVALID_SOCKET;
     }
 
-    SOCKET sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sockfd == INVALID_SOCKET)
+    SOCKET sockfd = INVALID_SOCKET;
+    for (ADDRINFOT* pinfo = aiList; pinfo != NULL; pinfo = pinfo->ai_next)
     {
-        _tprintf(_T("socket() failed, %s"), LAST_ERROR_MSG);
-        return 0;
+        sockfd = socket(pinfo->ai_family, pinfo->ai_socktype, pinfo->ai_protocol);
+        if (sockfd == INVALID_SOCKET)
+        {
+            fprintf(stderr, ("socket() failed, %s"), LAST_ERROR_MSG);
+            continue;
+        }
+        int error = bind(sockfd, pinfo->ai_addr, pinfo->ai_addrlen);
+        if (error == SOCKET_ERROR)
+        {
+            fprintf(stderr, ("bind() failed, addr: %s, len: %d, %s"), 
+                pinfo->ai_addr, pinfo->ai_addrlen, LAST_ERROR_MSG);
+            closesocket(sockfd);
+            sockfd = INVALID_SOCKET;
+            continue;
+        }
+        error = listen(sockfd, SOMAXCONN);
+        if (error == SOCKET_ERROR)
+        {
+            fprintf(stderr, ("listen() failed, %s"), LAST_ERROR_MSG);
+            closesocket(sockfd);
+            continue;
+        }
+        // 需要将套接字设置为非阻塞模式
+        ULONG nonblock = 1;
+        if (ioctlsocket(sockfd, FIONBIO, &nonblock) == SOCKET_ERROR)
+        {
+            fprintf(stderr, ("ioctlsocket() failed, %s"), LAST_ERROR_MSG);
+            closesocket(sockfd);
+            return INVALID_SOCKET;
+        }
+        break;
     }
 
-    int error = bind(sockfd, (sockaddr*)&addr, sizeof(addr));
-    if (error == SOCKET_ERROR)
-    {
-        _tprintf(_T("bind() failed, %s"), LAST_ERROR_MSG);
-        closesocket(sockfd);
-        return INVALID_SOCKET;
-    }
-
-    error = listen(sockfd, SOMAXCONN);
-    if (error == SOCKET_ERROR)
-    {
-        _tprintf(_T("listen() failed, %s"), LAST_ERROR_MSG);
-        closesocket(sockfd);
-        return INVALID_SOCKET;
-    }
-
-    // set socket to non-blocking mode
-    ULONG nonblock = 1;
-    if (ioctlsocket(sockfd, FIONBIO, &nonblock) == SOCKET_ERROR)
-    {
-        _tprintf(_T("ioctlsocket() failed, %s"), LAST_ERROR_MSG);
-        closesocket(sockfd);
-        return INVALID_SOCKET;
-    }
-
-    _tprintf(_T("server listen at %s, %s.\n"), strAddr.data(), Now().data());
+    freeaddrinfo(aiList);
+    fprintf(stdout, ("server listen at %s:%s, %s.\n"), host, port, Now().data());
     return sockfd;
 }
+
+
+bool select_loop(SOCKET sockfd)
+{
+    fd_set readset = {}; 
+
+    // 添加监听套接字
+    FD_SET(sockfd, &readset);
+
+    // 添加客户端套接字 
+    for (std::set<SOCKET>::const_iterator iter = g_total_sockets.begin();
+        iter != g_total_sockets.end(); ++iter)
+    {
+        FD_SET(*iter, &readset);
+    }
+
+    // 50毫秒的超时
+    timeval timeout = {0, 500*1000};
+    int nready = select(0, &readset, NULL, NULL, &timeout);
+    if (nready == SOCKET_ERROR)
+    {
+        fprintf(stderr, ("select() failed, %s"), LAST_ERROR_MSG);
+        return false;
+    }
+    if (nready == 0) // 超时处理
+    {            
+        return true;
+    }
+
+    // 检查接收数据
+    std::set<SOCKET>::iterator iter = g_total_sockets.begin();
+    while (iter != g_total_sockets.end())
+    {
+        SOCKET fd = *iter;
+        if (FD_ISSET(fd, &readset))
+        {
+            if (!on_recv(fd))
+            {
+                on_close(fd); // 失败后关闭连接
+                iter = g_total_sockets.erase(iter);
+                continue;
+            }
+        }
+        ++iter;
+    }
+
+    // 是否有新连接
+    if (FD_ISSET(sockfd, &readset))
+    {
+        on_accept(sockfd);
+    }
+
+    return true;
+}
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, ("Usage: select [host] [port]\n"));
+        return 1;
+    }
+
+    WinsockInit init;
+    SOCKET sockfd = create_listen_socket(argv[1], argv[2]);
+    if (sockfd == INVALID_SOCKET)
+    {
+        return 1;
+    }
+    
+    while (select_loop(sockfd))
+    {        
+    }
+
+    closesocket(sockfd); // close the listen socket
+    return 0;
+}
+
