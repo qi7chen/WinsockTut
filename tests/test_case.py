288d7ca4aa9ab1ca6c41cd9a24c50796d764e19f
johnnie chan <ichenq@gmail.com>
2013-12-15 01:07:01 +0800

    python测试客户端

--- tests/test_case.py
+++ tests/test_case.py
@@ -3,47 +3,45 @@
 
 import socket
 import select
+import time
 import pdb
 
-def fd_handler(fd):
-    msg = fd.recv(1024)
-    if len(msg) == 0: 
-        fd.close()
-    else:
-        print(msg)
-        fd.send(msg)
+# select循环
+def event_loop(read_list):
+    while read_list:
+        rs, ws, es = select.select(read_list, [], [], 3)
+        if not (rs or ws or es):
+            print('timed out')
+            continue
+        for s in rs:
+            data = s.recv(1024)
+            print(s.fileno(), data)
+            s.send(data)
+            time.sleep(1)
 
-def create_connections(host, port, count):
-    mydic = {}
-    for x in range(count):
-        s = socket.create_connection((host, port))
-        s.setblocking(0)
-        mydic[s] = fd_handler
-    return mydic
+# 创建一定数量的客户端连接
+def create_client(endpoint, count):
+    msg = 'the quick fox jumps over a lazy dog'
+    clients = []    
+    for i in range(count):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(endpoint)
+        s.setblocking(False)
+        s.send(msg)        
+        clients.append(s)
+    return clients
 
-def dispatch(mydict):
-    readlist = mydict.keys()
-    while True:
-        infds, outfds, errfds = select.select(readlist, [], [], 0)
-        if len(infds) == 0: break
-        for fd in infds:
-            handler = mydict.get(fd)
-            if handler: 
-                handler(fd)
-            else: 
-                fd.close()
-    
 
-def test_case(host, port, count):
-    msg = 'a quick fox jumps over the lazy dog\n'
-    mydict = create_connections(host, port, count)
-    for s in mydict.keys():
-        s.send(msg)
-    dispatch(mydict)
+def run_test():
+    host = '127.0.0.1'
+    port = 3245
+    count = 10
+    clients = create_client((host, port), count)
+    event_loop(clients)
+
 
 if __name__ == '__main__':
-    host = '192.168.0.9'
-    port = 3245
-    count = 1
-    test_case(host, port, count)
+    run_test()
+
     
+
