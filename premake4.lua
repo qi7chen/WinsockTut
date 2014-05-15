c84dab3899b0fb011c4da537d2d74bcf2cc791e6
ichenq <ichenq@gmail.com>
2014-05-15 12:50:30 +0800

    include dir and default defined macro

--- premake4.lua
+++ premake4.lua
@@ -20,6 +20,12 @@ solution 'WinsockExamples'
         location 'build'
         kind 'ConsoleApp'
         uuid "AB7D1C15-7A44-41a7-8864-230D8E345608"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }
         files 
         {
             'src/common/*.h',
@@ -27,11 +33,21 @@ solution 'WinsockExamples'
             'src/socket/*.h',
             'src/socket/*.cpp',
         }
+        includedirs
+        {
+            'src',
+        }
         
     project 'Select'
         location 'build'
         kind 'ConsoleApp'
         uuid "8701594A-72B8-4a6a-AEF3-6B41BBC33E65"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -39,12 +55,21 @@ solution 'WinsockExamples'
             'src/select/*.h',
             'src/select/*.cpp',
         }
-
+        includedirs
+        {
+            'src',
+        }
 
     project 'AsyncSelect'
         location 'build'
         kind 'ConsoleApp'
         uuid "83CA7514-377C-4957-8399-9E407CFDD8DF"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -52,11 +77,21 @@ solution 'WinsockExamples'
             'src/async_select/*.h',
             'src/async_select/*.cpp',
         }
+        includedirs
+        {
+            'src',
+        }        
 
     project 'AsyncEvent'
         location 'build'
         kind 'ConsoleApp'
         uuid "DB1835D4-DA1F-4499-8F21-6E1913C2122E"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -64,11 +99,21 @@ solution 'WinsockExamples'
             'src/async_event/*.h',
             'src/async_event/*.cpp',
         }
+        includedirs
+        {
+            'src',
+        }        
 
     project 'CompleteRoutine'
         location 'build'
         kind 'ConsoleApp'
         uuid "729FA509-952E-41f2-A33C-1E061FADE78A"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -76,11 +121,21 @@ solution 'WinsockExamples'
             'src/complete_routine/*.h',
             'src/complete_routine/*.cpp',
         }
+        includedirs
+        {
+            'src',
+        }        
 
     project 'Overlapped'
         location 'build'
         kind 'ConsoleApp'
         uuid "18A51657-E2D5-49a0-B3C7-FE2BE55AD98A"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -88,11 +143,21 @@ solution 'WinsockExamples'
             'src/overlapped/*.h',
             'src/overlapped/*.cpp',
         } 
+        includedirs
+        {
+            'src',
+        }        
         
     project 'IOCP'
         location 'build'
         kind 'ConsoleApp'
         uuid "588E072A-B1B8-4b36-B9BC-0E82547C7344"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
@@ -100,7 +165,10 @@ solution 'WinsockExamples'
             'src/iocp/*.h',
             'src/iocp/*.cpp',
         }
-        
+        includedirs
+        {
+            'src',
+        }
         
 solution 'TestClient'
     configurations {'Debug', 'Release'}    
@@ -120,6 +188,12 @@ solution 'TestClient'
         location 'build'
         kind 'ConsoleApp'
         uuid "FC0C8C22-35CF-4ce4-9A90-F99AD63A7BEA"
+        defines
+        {
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0501',
+            'NOMINMAX',
+        }        
         files 
         {
             'src/common/*.h',
