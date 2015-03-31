6a8a6ba6a926eab3bf51dc9d4080d11b274799aa
ichenq <ichenq@gmail.com>
2015-03-31 14:36:38 +0800

    Update copyright

--- tests/test_tcp_client.py
+++ tests/test_tcp_client.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#
+# Copyright (C) 2012-2015 ichenq@outlook.com. All rights reserved.
+# Distributed under the terms and conditions of the Apache License. 
+# See accompanying files LICENSE.
+#
+
 import socket
 import asyncore
 import random
