29d42e7da7a0a14d8be5d5552621073aebdcb773
ichenq <ichenq@gmail.com>
2015-04-11 20:23:33 +0800

    Update utility

--- src/common/utility.c
+++ src/common/utility.c
@@ -8,9 +8,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define THREAD_LOCAL    __declspec(thread)
+
 const char*  Now()
 {
-    static TLS char buffer[MAX_PATH];
+    static THREAD_LOCAL char buffer[MAX_PATH];
     struct tm st;
     time_t now = time(NULL);
     localtime_s(&st, &now);
@@ -20,7 +22,7 @@ const char*  Now()
 
 const char* GetErrorMessage(DWORD dwErrorCode)
 {
-    static TLS char description[MAX_PATH];
+    static THREAD_LOCAL char description[MAX_PATH];
     FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwErrorCode,
         0, description, MAX_PATH-1, NULL);
     return description;
--- src/common/utility.h
+++ src/common/utility.h
@@ -8,14 +8,9 @@
 
 #include <stdlib.h>
 #include <Windows.h>
-#include <WinSock2.h>
-
-#define TLS     __declspec(thread)
-#define INLINE  __inline
-
 
 /* default I/O buffer size */
-enum { kDefaultBufferSize = 8192 };
+#define  DEFAULT_BUFFER_SIZE 8192
 
 /* type of I/O operation */
 enum OperType
@@ -28,47 +23,18 @@ enum OperType
     OperDisconnect,     /* disconnect an socket */
 };
 
-/* per-handle data */
-typedef struct _PER_HANDLE_DATA 
-{
-    WSAOVERLAPPED   overlap;
-    SOCKET          socket;
-    WSABUF          wsbuf;
-    char            buffer[kDefaultBufferSize];
-    enum OperType   opertype;
-}PER_HANDLE_DATA;
-
+#define DEFAULT_HOST    "0.0.0.0"
+#define DEFAULT_PORT    "9527"
 
 /* current date */
-const char*     Now();
+const char* Now();
 
 /* description of specified error id */
-const char*     GetErrorMessage(DWORD dwError);
-
-
-/* create I/O completion port handle */
-INLINE HANDLE   CreateCompletionPort(DWORD dwConcurrency)
-{
-    return CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, dwConcurrency);
-}
-
-/* associate device handle to I/O completion port */
-INLINE int AssociateDevice(HANDLE hCompletionPort, HANDLE hDevice, ULONG_PTR completionkey)
-{    
-    HANDLE handle = CreateIoCompletionPort(hDevice, hCompletionPort, completionkey, 0);
-    return (handle == hCompletionPort);
-}
+const char*  GetErrorMessage(DWORD dwError);
 
 /* error description of current thread */
-#define LAST_ERROR_MSG      GetErrorMessage(GetLastError())
-
+#define LAST_ERROR_MSG   GetErrorMessage(GetLastError())
 
 #define CHECK(expr)   if (!(expr)) {                    \
     MessageBoxA(NULL, #expr, LAST_ERROR_MSG, MB_OK);    \
     abort(); }
-
-#define DEFAULT_HOST    "127.0.0.1"
-#define DEFAULT_PORT    "32450"
-
-
-
