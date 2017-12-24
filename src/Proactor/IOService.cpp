44e8e4a1be506b7dcdad16b7ef4fe0add733b93d
ichenq <ichenq@outlook.com>
2017-12-25 00:26:27 +0800

    add file

---
+++ src/Proactor/IOService.cpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2017 ichenq@outlook.com. All rights reserved.
+// Distributed under the terms and conditions of the Apache License. 
+// See accompanying files LICENSE.
+
+#include "IOService.h"
+
+IOService::IOService()
+{
+}
+
+IOService::~IOService()
+{
+}
